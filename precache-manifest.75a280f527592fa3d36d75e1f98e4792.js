41320078197f3ba589dd86fe5d09c06afd4ef2fd
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-05-23 01:29:35 -0400

    Updates

---
+++ precache-manifest.75a280f527592fa3d36d75e1f98e4792.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80e1340def0836e5675d8e4b83c33cf",
+    "url": "/give-me-advice-react-app/index.html"
+  },
+  {
+    "revision": "45802e8666efa679bbcb",
+    "url": "/give-me-advice-react-app/static/css/main.824bf054.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/give-me-advice-react-app/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "45802e8666efa679bbcb",
+    "url": "/give-me-advice-react-app/static/js/main.d9f833c3.chunk.js"
+  },
+  {
+    "revision": "e1daffdc2b7c483a12f1",
+    "url": "/give-me-advice-react-app/static/js/runtime~main.0159ab15.js"
+  }
+]);
